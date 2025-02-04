a89a32814ca7824e54d3e778be8d08a5e71a0ffb
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-02-04 16:46:00 +0530

    fix: update deployment script to remove NVM installation and pnpm setup; refactor RoomCanvas component to retrieve token within useEffect

--- apps/frontend/components/RoomCanvas.tsx
+++ apps/frontend/components/RoomCanvas.tsx
@@ -6,9 +6,10 @@ import { Canvas } from "./Canvas";
 
 export const RoomCanvas = ({ roomId }: { roomId: string }) => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const token = localStorage.getItem("token");
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
+
     const ws = new WebSocket(`${WS_URL}?token=${token}`);
 
     ws.onopen = () => {
@@ -20,7 +21,7 @@ export const RoomCanvas = ({ roomId }: { roomId: string }) => {
         })
       );
     };
-  }, [roomId, token]);
+  }, [roomId]);
 
   if (!socket) {
     return <div>Connecting to server....</div>;
