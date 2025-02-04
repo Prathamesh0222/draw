6e13f90b419af191b65bb0f5f30d5dd029193189
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-02-05 02:58:18 +0530

    fix: update HTTP and WS URLs to use environment variables for better configuration management

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,5 +1,5 @@
-export const HTTP_URL = "http://localhost:3001";
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_URL = process.env.NEXT_PUBLIC_HTTP_URL;
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 export const SIGNUP_IMG =
   "https://images.unsplash.com/photo-1599252441131-5aafffcf7740?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 export const SIGNIN_IMG =
