3ddb2fcac12632106eef8b9c90c5cf2b25b15fa6
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-02-13 02:16:43 +0530

    lint fix

--- apps/frontend/app/lib/auth.ts
+++ apps/frontend/app/lib/auth.ts
@@ -1,84 +1,86 @@
-import GoogleProvider from "next-auth/providers/google"
-import { prismaClient } from "@repo/db/client" 
-import { JWT_SECRET } from "@repo/backend-common/config"
+import GoogleProvider from "next-auth/providers/google";
+import { prismaClient } from "@repo/db/client";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
-import { Account, Profile, User } from "next-auth"
-import { JWT } from "next-auth/jwt"
-import { Session } from "next-auth"
+import { User } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { Session } from "next-auth";
 
 declare module "next-auth" {
-    interface User {
-        backendToken?: string;
-    }
-    interface Session {
-        backendToken?: string;
-    }
+  interface User {
+    backendToken?: string;
+  }
+  interface Session {
+    backendToken?: string;
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
-        backendToken?: string;
-    }
+  interface JWT {
+    backendToken?: string;
+  }
 }
 
 export const authOptions = {
-sessions: {
-    strategy: "jwt"
-},
-providers:[
+  sessions: {
+    strategy: "jwt",
+  },
+  providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID || "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    })
-],
-callbacks: {
-    async signIn({user, account, profile}: { user: User, account: Account | null, profile?: Profile }){
-    if(!user.email) return false;
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      if (!user.email) return false;
 
-    try {
+      try {
         let dbUser = await prismaClient.user.findFirst({
-            where: {
-                email: user.email
-            }
-        })
-        if(!dbUser){
-            dbUser = await prismaClient.user.create({
-                data: {
-                    email: user.email,
-                    username: user.name || "",
-                    password: ""
-                }
-            })
+          where: {
+            email: user.email,
+          },
+        });
+        if (!dbUser) {
+          dbUser = await prismaClient.user.create({
+            data: {
+              email: user.email,
+              username: user.name || "",
+              password: "",
+            },
+          });
         }
 
-        const backendToken = jwt.sign({
-            userId: dbUser.id
-        }, JWT_SECRET)
+        const backendToken = jwt.sign(
+          {
+            userId: dbUser.id,
+          },
+          JWT_SECRET
+        );
 
         user.backendToken = backendToken;
 
         return true;
-
-    } catch (error) {
-        console.error("Error in signIn callback", error)
+      } catch (error) {
+        console.error("Error in signIn callback", error);
         return false;
-    }
-  },
-  async session({session, token}: {session: Session, token: JWT}){
-    if(session.user){
+      }
+    },
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session.user) {
         session.backendToken = token.backendToken;
-    }
-    return session;
-  },
-  async jwt({token, user}: {token: JWT, user?: User}){
-    if(user){
+      }
+      return session;
+    },
+    async jwt({ token, user }: { token: JWT; user?: User }) {
+      if (user) {
         token.backendToken = user.backendToken;
-    }
-    return token;
-  }
-},
-pages: {
+      }
+      return token;
+    },
+  },
+  pages: {
     signIn: "/auth/signin",
-    error: "/auth/error"
- },
-}
+    error: "/auth/error",
+  },
+};
