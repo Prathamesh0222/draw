65c2b8d40ad91a132196455f91b2539abf4ac28e
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-02-15 15:08:54 +0530

    lint fix

--- apps/frontend/app/dashboard/loading.tsx
+++ apps/frontend/app/dashboard/loading.tsx
@@ -1,5 +1,3 @@
-import { Loader2 } from "lucide-react";
-
 export default function LoadingSkeleton() {
   return (
     <div className="p-6 border border-white/10 rounded-xl h-[150px] bg-white/5 backdrop-blur-lg shadow-xl flex flex-col justify-center items-center gap-4">
